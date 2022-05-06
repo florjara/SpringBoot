704f5604c1d61d6a34f8c5cd40a3752f95a8a911
Flor Jara <florenciajara96@gmail.com>
2022-05-06 19:50:29 -0300

    2do: controladores, vistas y metodos 'crear' de c/u

--- src/main/java/edu/egg/library/controller/EditorialController.java
+++ src/main/java/edu/egg/library/controller/EditorialController.java
@@ -1,11 +1,14 @@
 package edu.egg.library.controller;
 
+import edu.egg.library.entity.Editorial;
 import edu.egg.library.service.EditorialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/editorial")
@@ -19,5 +22,12 @@ public class EditorialController {
         ModelAndView mav = new ModelAndView("tabla-edit-autor");
         mav.addObject("editoriales", editorialService.obtenerEditoriales());
         return mav;
+    }
+    
+        @PostMapping("/crear-editorial")
+    public RedirectView crearAutor(Editorial editorialDto) {
+        RedirectView redirect = new RedirectView("/editorial");
+        editorialService.crearEditorial(editorialDto);
+        return redirect;
     }
 }
